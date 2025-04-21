74f8e07227228381c0f2aabed550c57179f8736d
Zalain Spain <2023159874@ub.edu.bz>
2025-04-21 00:26:45 +0000

    added Log all messages from each client feature

--- TCP-echo-server/main.go
+++ TCP-echo-server/main.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"bufio" //for buffered line reading
 	"strings" //to trim whitespace
+	"os" //for file writing
 )
 
 func main() {
@@ -38,6 +39,18 @@ func handleConnection(conn net.Conn) {
 	clientAddr := conn.RemoteAddr().String() //get client IP
 	fmt.Printf("[%s] Connected: %s\n", time.Now().Format(time.RFC3339), clientAddr) //log address and timestamp in UTC
 
+	//get the ip address for the file name
+	clientIP := strings.Split(clientAddr, ":") [0]
+	logFileName := clientIP + ".log"
+
+	//open the file or create if one doesn't exist
+	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("[%s] Error creating log file for %s: %v\n", time.Now().Format(time.RFC3339), clientAddr, err)
+		return
+	}
+	defer logFile.Close() // close file when its done
+
 	reader := bufio.NewReader(conn) //buffered reader to read lines
 
 	for {
@@ -51,10 +64,21 @@ func handleConnection(conn net.Conn) {
 		}
 			return
 		}
+
 		clean := strings.TrimSpace(line) //trim whitespace
+
+		//log message to the file
+		logEntry := fmt.Sprintf("[%s] %s\n", time.Now().Format(time.RFC3339),clean)
+		_, err = logFile.WriteString(logEntry)
+		if err != nil {
+			fmt.Printf("[%s] Error writing to log file for %s: %v\n", time.Now().Format(time.RFC3339), clientAddr, err)
+		}
+
+
 		_, err = conn.Write([]byte(clean + "\n")) // echo clean line
 		if err != nil {
 			fmt.Printf("[%s] Error writing to %s: %v\n", time.Now().Format(time.RFC3339), clientAddr, err)
+			return
 		}
 	}
 }
